dbababbaa3e21bbe8666a720511c0e52f2eb836d
6AHK0MAT <makhmutov.va@mail.ru>
2018-05-29 16:58:29 +0700

    Создание анимации

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div class="container text-center pt-5">
     <button class="btn btn-primary mb-5" @click="show = !show">Toggle</button>
 
-    <transition name="ma">
+    <transition
+      name="ma"
+      type="animation"
+    >
       <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, minus?</p>
     </transition>
   </div>
@@ -20,13 +23,31 @@
 
 <style>
 
-  .ma-enter {}
-  .ma-enter-active {}
+  .ma-enter {
+    opacity: 0;
+  }
+  .ma-enter-active {
+    transition: opacity 1s;
+  }
   .ma-enter-to {}
 
   .ma-leave {}
-  .ma-leave-active {}
-  .ma-leave-to {}
+  .ma-leave-active {
+    animation: 1s ma-slide forwards;
+    transition: opacity 3s;
+  }
+  .ma-leave-to {
+    opacity: 0;
+  }
+
+  @keyframes ma-slide {
+    from {
+      transform: translateX(0px);
+    }
+    to {
+      transform: translateX(-100px);
+    }
+  }
 
 
 </style>
