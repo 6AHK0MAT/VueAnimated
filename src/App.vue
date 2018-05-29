bec8f749d65858bc9c43086ede8cf82e3baa0f57
6AHK0MAT <makhmutov.va@mail.ru>
2018-05-29 11:55:27 +0700

    Этапы анимации

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div class="container text-center pt-5">
     <button class="btn btn-primary mb-5" @click="show = !show">Toggle</button>
 
-    <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, minus?</p>
+    <transition name="ma">
+      <p v-if="show">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, minus?</p>
+    </transition>
   </div>
 </template>
 
@@ -17,5 +19,14 @@
 </script>
 
 <style>
+
+  .ma-enter {}
+  .ma-enter-active {}
+  .ma-enter-to {}
+
+  .ma-leave {}
+  .ma-leave-active {}
+  .ma-leave-to {}
+
 
 </style>
